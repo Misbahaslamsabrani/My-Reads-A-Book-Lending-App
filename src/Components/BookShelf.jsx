b3c31aaff677ebe9b17e1d60a4581d7ecfaf24f2
Misbahaslamsabrani <misbahaslamsabrani@gmail.com>
2019-03-11 20:01:43 +0500

    show and update books on shelf

--- src/Components/BookShelf.jsx
+++ src/Components/BookShelf.jsx
@@ -1,112 +1,132 @@
 import React, { Component } from 'react';
-/* import * as BooksAPI from './BooksAPI' */
+import * as BooksAPI from '../BooksAPI';
 class BookShelf extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {
+            Read: [],
+            CurrentlyRead: [],
+            WantToRead: [],
+        };
+    }
+    componentDidMount() {
+         this.getData()
+    }
+    getData = () => {
+        BooksAPI.getAll().then(data => {
+            console.log(data)
+            const CurrentlyRead = data.filter(v => v.shelf === "currentlyReading")
+            const Read = data.filter(v => v.shelf === "read")
+            const WantToRead = data.filter(v => v.shelf === "wantToRead")
+            this.setState({ CurrentlyRead, Read, WantToRead })
+        })
+    }
+    whenSelect = (event, book) => {
+        console.log(event.target)
+        const shelf = event.target.value;
+        BooksAPI.update(book, shelf).then(data => {
+            this.getData()
+        })
     }
     render() {
         return (
             <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-
-            <div className="list-books-content">
-              <div>
-
-                {/* -----------------Currently reading ----------------------------------------------------- */}
-
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        {/* -----------------Currently reading ----------------------------------------------------- */}
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Currently Reading</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {this.state.CurrentlyRead.length > 0 ? (this.state.CurrentlyRead.map(v => <li key={v.id}>
+                                        <div className="book">
+                                            <div className="book-top">
+                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${v.imageLinks.thumbnail})` }}></div>
+                                                <div className="book-shelf-changer">
+                                                    <select onChange={(e) => this.whenSelect(e, v)} defaultValue={v.shelf}>
+                                                        <option value="move" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div className="book-title">{v.title}</div>
+                                            <div className="book-authors">{v.authors.join(", ")}</div>
+                                        </div>
+                                    </li>)) : (<li> No Books Found </li>)}
+                                </ol>
                             </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
                         </div>
-                      </li>
-                    </ol>
-                  </div>
+
+                        {/* ----------------------------------WANT TO READ----------------------------------------------------------*/}
+
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Want to Read</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {this.state.WantToRead.length > 0 ? (this.state.WantToRead.map(v => <li key={v.id}>
+                                        <div className="book">
+                                            <div className="book-top">
+                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${v.imageLinks.thumbnail})` }}></div>
+                                                <div className="book-shelf-changer">
+                                                    <select onChange={(e) => this.whenSelect(e, v)} defaultValue={v.shelf}>
+                                                        <option value="move" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div className="book-title">{v.title}</div>
+                                            <div className="book-authors">{v.authors.join(", ")}</div>
+                                        </div>
+                                    </li>)) : (<li> No Books Found </li>)}
+                                </ol>
+                            </div>
+                        </div>
+
+                        {/* ------------------------------READ---------------------------------------------------------------- */}
+
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Read</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {this.state.Read.length > 0 ? (this.state.Read.map(v => <li key={v.id}>
+                                        <div className="book">
+                                            <div className="book-top">
+                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${v.imageLinks.thumbnail})` }}></div>
+                                                <div className="book-shelf-changer">
+                                                    <select onChange={(e) => this.whenSelect(e, v)} defaultValue={v.shelf}>
+                                                        <option value="move" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div className="book-title">{v.title}</div>
+                                            <div className="book-authors">{v.authors.join(", ")}</div>
+                                        </div>
+                                    </li>)) : (<li> No Books Found </li>)}
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                {/* ----------------------------------WANT TO READ----------------------------------------------------------*/}
-                
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">1776</div>
-                          <div className="book-authors">David McCullough</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
+                {/*-----------------------------------------------------SEARCH BUTTON/ADD BUTTON------------------------------ */}
+
+                <div className="open-search">
+                    <button onClick={() => this.props.history.push("/Search")}>Add a book</button>
                 </div>
-
-                {/* ------------------------------READ---------------------------------------------------------------- */}
-
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">The Hobbit</div>
-                          <div className="book-authors">J.R.R. Tolkien</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
-              </div>
             </div>
-
-            {/*-----------------------------------------------------SEARCH BUTTON/ADD BUTTON------------------------------ */}
-
-            <div className="open-search">
-              <button onClick={() => this.props.history.push("/Search")}>Add a book</button>
-            </div>
-          </div>
         );
     }
 }
