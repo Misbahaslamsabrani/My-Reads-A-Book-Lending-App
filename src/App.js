7652445b9cdf93e0a4797e39601eebc8de901174
Misbahaslamsabrani <misbahaslamsabrani@gmail.com>
2019-03-16 21:23:56 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Search from './Components/Search';
 import BookShelf from './Components/BookShelf';
+import { getAll, update } from './BooksAPI';
 
-class BooksApp extends Component{
+class BooksApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Read: [],
+      CurrentlyRead: [],
+      WantToRead: [],
+    };
+  }
+  componentDidMount() {
+    this.getData()
+  }
+  getData = () => {
+    getAll().then(data => {
+      let CurrentlyRead = [];
+      let Read = [];
+      let WantToRead = [];
+      data.map(v => {
+        const { shelf } = v;
+        switch (shelf) {
+          case "currentlyReading":
+            CurrentlyRead.push(v)
+            return null;
+          case "wantToRead":
+            WantToRead.push(v)
+            return null;
+          case "read":
+            Read.push(v)
+            return null;
+          default:
+            return null;
+        }
+      })
+      this.setState({ CurrentlyRead, Read, WantToRead })
+    })
+  }
+    /* whenSelect = (event, book) => {
+    let shelf = event.target.value;
+    update(book, shelf).then(data => {
+      switch(shelf){
+        case "read":
+        const temArr = [...this.state.Read];
+        temArr.push(book)
+        const index = this.state.WantToRead.findIndex(v => v.id === book.id)
+        index && 
+      }
+    })
+  }  */
   render() {
+    const { CurrentlyRead, WantToRead, Read } = this.state;
     return (
       <div className="app">
-      <Router>
+        <Router>
           <Switch>
-            <Route exact path="/" component={BookShelf}/>
-            <Route exact path="/Search" component={Search}/>
+            <Route exact path="/" render={() => <BookShelf
+              CurrentlyRead={[...CurrentlyRead]}
+              WantToRead={[...WantToRead]}
+              Read={[...Read]} 
+              whenSelect={this.whenSelect}/>}
+            />
+
+            <Route exact path="/Search" render={() => <Search
+              allCurrentSelfBooks={[...CurrentlyRead, ...WantToRead, ...Read]} />}
+            />
           </Switch>
-      </Router>
+        </Router>
       </div>
     )
   }
 }
-export default BooksApp
+export default BooksApp;
 
---
+++ src/Components/BookShelf.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Shelf from './Shelf';
+import { Link } from "react-router-dom";
+class BookShelf extends Component {
+    render() {
+        const { CurrentlyRead, WantToRead, Read, whenSelect } = this.props;
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <Shelf books={CurrentlyRead} title="Currently Reading" whenselected={whenSelect} />
+                        <Shelf books={WantToRead} title="Want To Read" whenselected={whenSelect} />
+                        <Shelf books={Read} title="Read" whenselected={whenSelect} />
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link to="/Search" >
+                        <button>Add a book</button>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BookShelf;
